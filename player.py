e48e8331b74930e60be172a417f09f84cdc2f5c3
miguel-l-b <cc22327@g.unicamp.br>
2023-11-23 17:07:14 -0300

    first commit

---
+++ player.py
@@ -0,0 +1,52 @@
+import pygame
+from shot import Shot
+from pygame.locals import *
+
+class Player():  
+  def __init__(self, name: str, color: pygame.Color, initialCoords = (0, 0), hp: int = 100, shape: str = "snake", hitbox = (40, 40)):
+    self.__hp = hp
+    self.__name = name
+    self.__color = color
+    self.__coords = initialCoords
+    self.__shape = shape
+    self.shots = []
+    self.__hitbox = pygame.Rect(self.__coords[0], self.__coords[1], hitbox[0], hitbox[1])
+  
+  @property
+  def coords(self):
+    return self.__coords
+  
+  @coords.setter
+  def coords(self, newCoords):
+    self.__coords = newCoords
+  
+  @property
+  def color(self):
+    return self.__color
+
+  @color.setter
+  def color(self, newColor):
+    self.__color = newColor
+
+  @property
+  def hp(self):
+    return self.__hp
+
+  @hp.setter
+  def hp(self, newHp):
+    self.__hp = newHp
+
+  def isInHitBox(self, coords):
+    return (coords[0] == self.__coords[0] + 40 or 
+            coords[0] == self.__coords[0] - 40 or 
+            coords[1] == self.__coords[1] + 40 or 
+            coords[1] == self.__coords[1] - 40)
+
+  def moveUp(self, distance):
+    self.__coords = (self.__coords[0], self.__coords[1] - distance)
+  
+  def moveDown(self, distance):
+    self.__coords = (self.__coords[0], self.__coords[1] + distance)
+
+  def shoot(self, color: pygame.Color, radius: float = 10, speed: int = 10):
+    self.shots.append(Shot(color, self.__coords, radius, speed))
---
+++ shot.py
@@ -0,0 +1,46 @@
+import pygame
+
+class Shot():
+    def __init__(self, color: pygame.color, initialCoords = (0, 0), radius: float = 10, speed: int = 10):
+        self.__color = color
+        self.__coords = initialCoords
+        self.__radius = radius
+        self.__speed = speed
+
+    @property
+    def coords(self):
+        return self.__coords
+
+    @coords.setter
+    def coords(self, newCoords):
+        self.__coords = newCoords
+
+    @property
+    def color(self):
+        return self.__color
+    
+    @color.setter
+    def color(self, newColor):
+        self.__color = newColor
+
+    @property
+    def radius(self):
+        return self.__radius
+
+    @radius.setter    
+    def radius(self, newRadius):
+        self.__radius = newRadius
+
+    @property
+    def speed(self):
+        return self.__speed
+
+    @speed.setter    
+    def speed(self, newSpeed):
+        self.__speed = newSpeed
+    
+    def moveLeft(self, distance):
+        self.__coords = (self.__coords[0] - distance, self.__coords[1])
+    
+    def moveRight(self, distance):
+        self.__coords = (self.__coords[0] + distance, self.__coords[1])
