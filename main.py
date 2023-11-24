1df2aa6d47f9f3422b98abd53c3fa125589fa5ff
miguel-l-b <miguellopesbraido@gmail.com>
2023-11-24 09:21:56 -0300

    Update game settings and main scene

--- main.py
+++ main.py
@@ -3,21 +3,26 @@ from core.resolver import ResolverConfig, ResolverFile, ResolverPath
 import importlib.util
 
 pygame.init()
-screen = pygame.display.set_mode((500, 500))
+pygame.display.set_caption(ResolverConfig.resolve()["window"]["title"])
+screen = pygame.display.set_mode(
+  ResolverConfig.resolve()["window"]["dimension"],
+  pygame.FULLSCREEN if ResolverConfig.resolve()["window"]["fullScreen"] else 0
+)
+main = ResolverConfig.resolve()["game"]["mainScene"]
 
-main = ResolverConfig.resolve()["mainScene"]
+for scene in ResolverFile.getAllFilesWithExtension("@scenes", ".py"):
+  name_scene = scene.replace(".py", "")
+  if name_scene == main:
+    spec = importlib.util.spec_from_file_location(
+      name_scene,
+      ResolverPath.resolve(f"@scenes/{scene}")
+    )
+    modulo = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(modulo)
 
-for scene in ResolverFile.getAllFiles("@scenes"):
-  if scene.endswith(".py"):
-    name_scene = scene.replace(".py", "")
-    if name_scene == main:
-      spec = importlib.util.spec_from_file_location(name_scene, ResolverPath.resolve(f"@scenes/{scene}"))
-      modulo = importlib.util.module_from_spec(spec)
-      spec.loader.exec_module(modulo)
-
-      # Agora, você pode usar o módulo normalmente
-      modulo.start(screen)
-      break
+    # Agora, você pode usar o módulo normalmente
+    modulo.start(screen)
+    break
 
 pygame.quit()
 exit()
