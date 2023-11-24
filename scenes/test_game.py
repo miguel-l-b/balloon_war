4861d8319297c83284b6ca4203df5c85a58b9c62
Bruno <brunoconcli@gmail.com>
2023-11-24 00:27:45 -0300

    Temporarily alter testing

--- scenes/test_game.py
+++ scenes/test_game.py
@@ -1,126 +1,110 @@
-from time import sleep
 import pygame
+from time import sleep
 from pygame.locals import *
 from entities.player import Player
-from entities.shot import Shot
-
-from entities.player import Player
-from pygame import *
 
 
-class testGame:
-  status = True
-  fps = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+p1 = Player("p1", (43, 65, 194), (450, 50))
+p2 = Player("p2", (128, 25, 41), (50, 450))
 
-  objects = []
+p1_shoot_interval = True
+p2_shoot_interval = True
 
-  # font_HUD_player = pygame.font.SysFont("Arial", 50)
-  # font_HUD_points = pygame.font.SysFont("Arial", 30)
-  # font_show_info = pygame.font.SysFont("Arial", 10)
+players = [p1, p2]
 
-  circle = {"x": 50, "y": 50, "direction": "right", "radius": 20}
+p1_shots = []
+p2_shots = []
 
-  p1 = Player("p1", (43, 65, 194), (450, 50))
-  p2 = Player("p2", (128, 25, 41), (50, 450))
+players_speed = 4
 
-  p1_shoot_interval = True
-  p2_shoot_interval = True
+hp_font = pygame.font.SysFont("Courier New", 24)
 
-  players = [p1, p2]
-
-  p1_shots = []
-  p2_shots = []
-
-  # font = pygame.font.SysFont("Courier New", 24)
-
-  def __init__(self):
-    pass
-
-
-  def render_objects(self, screen: Surface):
+def render_objects():
     screen.fill((255, 255, 255))
 
-    # p1_hp_display = font.render(f"Cyan's HP: {p1.hp}", True, (93, 133, 245))
-    # p2_hp_display = font.render(f"Oranges's HP: {p2.hp}", True, (222, 119, 51))
+    p1_hp_display = hp_font.render(f"Cyan's HP: {p1.hp}", True, (93, 133, 245))
+    p2_hp_display = hp_font.render(f"Oranges's HP: {p2.hp}", True, (222, 119, 51))
 
-    # screen.blit(p1_hp_display, (300, 10))
-    # screen.blit(p2_hp_display, (10, 10))
+    screen.blit(p1_hp_display, (300, 10))
+    screen.blit(p2_hp_display, (10, 10))
 
-    for player in testGame.players:
-      pygame.draw.circle(screen, player.color, player.coords, 40)
+    for player in players:
+        if (player.hp <= 0):
+            player.die()
+            players.remove(player)
+        else:
+            pygame.draw.circle(screen, player.color, player.coords, player.size)
 
-    for shot in testGame.p1.shots:
-      if (p2.isInHitBox(shot.coords)):
-        p2.hp -= 10
-        p1.shots.remove(shot)
-      
-      if (shot.coords[0] < 0):
-        p1.shots.remove(shot)
-      else:
-        pygame.draw.circle(screen, shot.color, shot.coords, shot.radius)
-        shot.coords = (shot.coords[0] - shot.speed, shot.coords[1])
+    if (p1.alive and p2.alive):
+        for shot in p1.shots:
+            # -- 
+            if p2.findShot(shot) != -1:
+                p1.shots.remove(shot)
+                p2.shot.remove(shot)
 
-    for shot in testGame.p2.shots:
-      if (p1.isInHitBox(shot.coords)):
-        p1.hp -= 10
-        p2.shots.remove(shot)
-      if (shot.coords[0] > 500):
-        p2.shots.remove(shot)
-      else:
-        pygame.draw.circle(screen, shot.color, shot.coords, shot.radius)
-        shot.coords = (shot.coords[0] + shot.speed, shot.coords[1])
-          
-  def start(self, screen: Surface):
-    while testGame.status:
-      self.render_objects(screen)
-      for event in pygame.event.get():
+            if (p2.hit(shot.coords)):
+                p2.hp -= 10
+                p1.shots.remove(shot)
+            
+            if (shot.coords[0] < 0):
+                p1.shots.remove(shot)
+            else:
+                pygame.draw.circle(screen, shot.color, shot.coords, shot.radius)
+                shot.coords = (shot.coords[0] - shot.speed, shot.coords[1])
+
+        for shot in p2.shots:
+            if (p1.hit(shot.coords)):
+                p1.hp -= 10
+                p2.shots.remove(shot)
+            if (shot.coords[0] > 500):
+                p2.shots.remove(shot)
+            else:
+                pygame.draw.circle(screen, shot.color, shot.coords, shot.radius)
+                shot.coords = (shot.coords[0] + shot.speed, shot.coords[1])
+                
+status = True
+while status:
+    render_objects()
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
-          testGame.status = False
-        
+            status = False
+    
         if event.type == pygame.KEYDOWN:
-          if event.key == pygame.K_UP:
-            if (p1.coords[1] > 20):
-              p1.moveUp(20)
+            if event.key == pygame.K_LEFT:
+                if (p1.alive):
+                    if (p1_shoot_interval):
+                        p1.shoot((60, 107, 201), 2)
+                    p1_shoot_interval = not p1_shoot_interval
 
-          if event.key == pygame.K_DOWN:
-            if (p1.coords[1] < 480):
-              p1.moveDown(20)
+            if event.key == pygame.K_d:
+                if (p2.alive):
+                    if (p2_shoot_interval):
+                        p2.shoot((199, 52, 42), 2)
+                p2_shoot_interval = not p2_shoot_interval
 
-          if event.key == pygame.K_LEFT:
-            if (p1_shoot_interval):
-              p1.shoot((60, 107, 201))
-            p1_shoot_interval = not p1_shoot_interval
+    keys = pygame.key.get_pressed()
 
-          if event.key == pygame.K_w:
-            if (p2.coords[1] > 20):
-              p2.moveUp(20)
-
-          if event.key == pygame.K_s:
-            if (p2.coords[1] < 480):
-              p2.moveDown(20)
-
-          if event.key == pygame.K_d:
-            if (p2_shoot_interval):
-              p2.shoot((199, 52, 42))
-            p2_shoot_interval = not p2_shoot_interval
+    if (p1.alive):
+        if keys[pygame.K_UP] and p1.coords[1] > 20:
+            p1.moveUp(players_speed)
+        if keys[pygame.K_DOWN] and p1.coords[1] < 480:
+            p1.moveDown(players_speed)
+    if (p2.alive):
+        if keys[pygame.K_w] and p2.coords[1] > 20:
+            p2.moveUp(players_speed)
+        if keys[pygame.K_s] and p2.coords[1] < 480:
+            p2.moveDown(players_speed) 
 
     pygame.display.update()
-    sleep(0.1)
-  # ---HUD---
-  # screen.blit(font_show_info.render('FPS: %.1f' % fps.get_fps(), 1, (100, 0, 50)), (10, 10))
-  # screen.blit(font_show_info.render('Time: ' + str(fps.get_time()), 1, (100, 0, 50)), (10, 20))
-  
-  # pygame.display.update()
-  # fps.tick(120)
-  
-  # if(circle["direction"] == "right" and circle["x"] < circle["radius"]):
-  #   print("right")
-  #   circle["x"] += 1
-  # elif(circle["x"] > screen.get_width() - circle["radius"]):
-  #   print("left")
-  #   circle["x"] -= 1
-  # else:
-  #   if(circle["direction"] == "right"):
-  #     circle["direction"] = "left"
-  #   else:
-  #     circle["direction"] = "right"
+    sleep(0.01)
+
+pygame.quit()
+exit()
+
+# FIX HITBOX -- check
+# SLOW DOW SHOTS -- check
+# MAKE KEY PRESS -- check
+# ABSTRACT
+# MAKE BULLET COLLISION 
