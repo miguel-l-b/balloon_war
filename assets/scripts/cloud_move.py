fdeed3f888e60a70128c31c8d0bdaa1f536f3e56
miguel-l-b <miguellopesbraido@gmail.com>
2023-11-25 20:42:15 -0300

    Add cloud generator and cloud movement scripts

--- assets/scripts/cloud_move.py
+++ assets/scripts/cloud_move.py
@@ -1,16 +1,39 @@
+import random
 import pygame
-from pygame import Vector2
-from core.entities import Entity
+from core.entities import Sprite
+from core.resolver import ResolverConfig
+from core.scene import Scene
 import core.types as types
 
 class CloudMove(types.Script):
-  def __init__(self):
+  def __init__(self, direction, scene: Scene, speed: int):
     super().__init__()
+    self.speed = speed
+    self.scene = scene
+    self.direction = direction
 
-  def setup(self, owner: Entity, data: types.TDataScript = None):
+  def setup(self, owner: Sprite):
     self.owner = owner
-    self.data = data
+    self.limits = ResolverConfig.resolve()["window"]["dimension"]
+    
+  def loop(self, _screen: pygame.Surface, delta_time: float):
+    distanceX = self.speed * delta_time
+    distanceY =  self.speed * delta_time
+    r = random.randint(0, 200)
+    if 99 > r and r < 110:
+      distanceY *= -0.2
+  
+    if 110 > r and r < 120:
+      distanceY *= 0.2
 
-  def loop(self, screen: pygame.Surface, delta_time: float):
-    move = Vector2(50, 10) * delta_time
-    self.owner.moving((move.x, move.y))
+    if r <= 99 or r >= 120:
+      distanceY *= 0
+    
+    if self.direction == 0:
+      self.owner.moving((distanceX, distanceY))
+      if self.owner.coords[0] > self.limits[0]:
+          self.scene.kill(self.owner.name)
+    else:
+      self.owner.moving((distanceX, distanceY))
+      if self.owner.coords[1] > self.limits[1]:
+          self.scene.kill(self.owner.name)
