0812277100273fc692730f863f4dd701f5b7844e
miguel-l-b <miguellopesbraido@gmail.com>
2023-11-25 23:34:14 -0300

    Add sprites and buttons to LobbyScene

--- scenes/lobby.py
+++ scenes/lobby.py
@@ -9,14 +9,18 @@ from core.sprite import SpriteSlicer
 class LobbyScene(Scene):
   def __init__(self, screen: Surface) -> None:
     super().__init__(screen)
-    sprites: list = [
-      SpriteSlicer(
-        ResolverPath.resolve("@sprites/title_game.png"),
-        {"width": 157, "height": 40, "rows": 5, "columns": 4},
-        18,
-        (157 * 2, 40 * 2)
-      )
-    ]
+    spriteTitle = SpriteSlicer(
+      ResolverPath.resolve("@sprites/title_game.png"),
+      {"width": 157, "height": 40, "rows": 5, "columns": 4},
+      18,
+      (157 * 2, 40 * 2)
+    )
+    spriteLongButtons = SpriteSlicer(
+      ResolverPath.resolve("@sprites/long_buttons.png"),
+      {"width": 120, "height": 50, "rows": 1, "columns": 2},
+      2,
+      (120 * 2, 50 * 2)
+    )
     
     zTop = types.zGroup(1, "top")
     zBottom = types.zGroup(-1, "bottom")
@@ -25,9 +29,9 @@ class LobbyScene(Scene):
     self.spawn(
       AnimatedSprite(
         "title",
-        ResolverCoords.getCoordsWithCenterX(ResolverConfig.resolve()["window"]["dimension"], sprites[0].size),
+        ResolverCoords.getCoordsWithCenterX(ResolverConfig.resolve()["window"]["dimension"], spriteTitle.size),
         zMedium,
-        sprites[0],
+        spriteTitle,
         10,
         rollback=True,
         stopWithSprite=18,
@@ -58,6 +62,16 @@ class LobbyScene(Scene):
         ]
       )
     )
+
+    self.spawn(
+      Sprite(
+        "btn_play",
+        (ResolverCoords.getCoordsWithCenter(ResolverCoords.getSizeScreen(), spriteLongButtons.size)),
+        zTop,
+        spriteLongButtons.get(0)
+      )
+    )
+
 
 def start(screen: Surface):
   pygame.mixer.music.load(ResolverPath.resolve("@audio/music/retro_music_in_game.wav"))
