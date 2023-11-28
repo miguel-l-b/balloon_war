554e9028079ff10aa70295d1d7f06b5cfa8eb0c8
miguel-l-b <miguellopesbraido@gmail.com>
2023-11-27 23:32:59 -0300

    Fix thread name and stop load scene

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ main = ResolverConfig.resolve()["game"]["mainScene"]
 
 if load is not None:
   load_scene: SceneLoader = ResolverScene.handleScene(load, screen)
-  thread_load = threading.Thread(target=load_scene.start)
+  thread_load = threading.Thread(target=load_scene.start, name=load_scene.__class__.__name__)
   thread_load.start()
   manager = ManagerScenes(screen)
-  while not load_scene.isFinished:
-    continue
+  load_scene.stop()
+  thread_load.join()
   manager.goTo(main)
 else:
   ManagerScenes(screen).goTo(main)
